be677df325774f0e006d89238b22c79f9f0c20f6
kshelton7821 <keaton.shelton2@gmail.com>
2023-02-07 20:49:35 -0600

    M1a and m1b final.

--- Lab1/m1a/m1a.c
+++ Lab1/m1a/m1a.c
@@ -1,14 +1,17 @@
 /*
 Original Author: Dr. J.W Bruce
+Modified by: Keaton Shelton
 Abstract:
    This is a simple program to blink an LED on the STM32L4 Discovery Board.
 
 
 Questions:
 1. What is the period of the LED blink? How do we know?
-Ans: The period is 1/7 of a second. We know this because the delay loop is
-     57143 cycles long and the clock speed is 4MHz. 57143/4000000 = 0.0142857
-     or 1/7 of a second. But we know this for sure because we measured it with an Analog Discovery 2 Oscilloscope.
+Ans: The period is 1 second. We know this for sure because we measured it with an Analog Discovery 2 Oscilloscope.
+   We were able to then predict the period using the classic equation of Time = IC / Freq. However we did have to modify this equation
+   to get accurate results. If plugging in for IC we get 400000 cycles and for Freq we get 4MHz. This gives us 100ms. However, we know that the
+   true period is 1 second. With this in mind we solved for Time(1s) = IC(400000) / Freq(4Mhz)*x. This gives us x = 0.1. Or more accurately 1/10.
+   Essentially reducing the frequency by a factor of 10. This is why we had to modify the equation to get accurate results.
 
 2. What is the frequency of the main system clock? How do we know? How could we physically verify that?
 Ans: The frequency of the main system clock is 4MHz. We know this because the startup clock speed is stated on P.34 of the STM32L452RE Data Sheet. 
@@ -17,15 +20,17 @@ Ans: The frequency of the main system clock is 4MHz. We know this because the st
 3. Examine the listing17 file (m1a.lst) created when m1a.c is compiled. Explain how to
 find the exact period of the blink from the listing file contents before you every flash the
 program to hardware and run it.
-Ans: We can see in the .lst file that the delay cycle is loaded as a word. This word is our delay interval.
+Ans: We can see in the .lst file that the delay cycle is loaded as a word. This word is our delay interval or cucle count.
+   We can then calculate our period by using our modified equation of Time(x) = IC(400000) / Freq(4Mhz)*(1/10). With x = 1s.
 
 4. Now, explain how to change the value in delay_loop() to get a LED flash period of exactly 1/7 second. Make the change and verify your effort.
 Ans: We know that a delay of 400000 cycles is 1 second. We want 1/7 of a second so we need to divide 400000 by 7. This gives us 57143 cycles.
-   We can see this in the .lst file.
+   We can see this in the .lst file. Using our modified equation of Time(x) = IC(57143) / Freq(4Mhz)*(1/10). With x = 0.142858 seconds.
+   This was then verified by measuring the period with an Analog Discovery 2 Oscilloscope.
 
 Revisions:
 01ks 2-2-2023 Add commments about clock speed and NOP cycle time.
-02ks 2-6-2023 Change Period to 1/7 of a second.
+02ks 2-6-2023 Change Period to 1/7 of a second and add values.
 */
 #ifndef STM32L4
    #define STM32L4
@@ -61,7 +66,7 @@ int main(void)
       //Toggle GPIOA5
        gpio_toggle(LED2_GPIO_Port, LED2_Pin);
        //Original Value = 400000
-       //Original Period = 1s
+       //Original Period = 1s 57143
        delay_loop(57143);
     }
 }
