06165226a31d8c13df233dc78f614719ef6b0668
Keaton Shelton <keaton.shelton2@gmail.com>
2023-04-13 22:02:26 -0500

    Fix main issues

--- M6/include/opmode.h
+++ M6/include/opmode.h
@@ -10,6 +10,7 @@
 * Revisions:
 * 01jw - 4/5/2023 - Initial version
 * 02ks - 4/5/2023 - Add fnst_opmodeInit
+* 03ks - 4/13/2023 - Cleanup
 */
 #ifndef OPMODE_H
 #define OPMODE_H
@@ -25,14 +26,10 @@
 
 
 typedef struct {
-    uint8_t u8_SW1;
-    uint8_t u8_SW2;
-    uint8_t u8_SW3;
-    uint8_t u8_SW4;
-    uint8_t u8_SW5;
+    uint16_t u16t_SW1, u16t_SW2, u16t_SW3, u16t_SW4, u16t_SW5;
 } st_opmode;
 
-void fnst_opmodeUpdate(st_opmode volatile *pst_opmode, uint8_t SW1, uint8_t SW2, uint8_t SW3, uint8_t SW4, uint8_t SW5);
+void fnst_opmodeUpdate(st_opmode volatile *pst_opmode, uint16_t u16t_SW1, uint16_t u16t_SW2, uint16_t u16t_SW3, uint16_t u16t_SW4, uint16_t u16t_SW5);
 void fnst_opmodeInit(st_opmode volatile *pst_opmode);
 uint8_t fnst_opmodePriority(st_opmode volatile *pst_opmode);
 
--- M6/src/opmode.c
+++ M6/src/opmode.c
@@ -16,30 +16,30 @@
 
 
 // Updates the opmode struct values
-void fnst_opmodeUpdate(st_opmode volatile *pst_opmode, uint8_t SW1, uint8_t SW2, uint8_t SW3, uint8_t SW4, uint8_t SW5) {
+void fnst_opmodeUpdate(st_opmode volatile *pst_opmode, uint16_t u16t_SW1, uint16_t u16t_SW2, uint16_t u16t_SW3, uint16_t u16t_SW4, uint16_t u16t_SW5) {
     // Read the switches via defines in main header file
-    pst_opmode->u8_SW1 = SW1;
-    pst_opmode->u8_SW2 = SW2;
-    pst_opmode->u8_SW3 = SW3;
-    pst_opmode->u8_SW4 = SW4;
-    pst_opmode->u8_SW5 = SW5;
+    pst_opmode->u16t_SW1 = u16t_SW1;
+    pst_opmode->u16t_SW2 = u16t_SW2;
+    pst_opmode->u16t_SW3 = u16t_SW3;
+    pst_opmode->u16t_SW4 = u16t_SW4;
+    pst_opmode->u16t_SW5 = u16t_SW5;
 }
 
 // Initialize the opmode struct values
 void fnst_opmodeInit(st_opmode volatile *pst_opmode) {
-    pst_opmode->u8_SW1 = 0;
-    pst_opmode->u8_SW2 = 0;
-    pst_opmode->u8_SW3 = 0;
-    pst_opmode->u8_SW4 = 0;
-    pst_opmode->u8_SW5 = 0;
+    pst_opmode->u16t_SW1 = 0;
+    pst_opmode->u16t_SW2 = 0;
+    pst_opmode->u16t_SW3 = 0;
+    pst_opmode->u16t_SW4 = 0;
+    pst_opmode->u16t_SW5 = 0;
 }
 
 // Return the highest priority operation.
 uint8_t fnst_opmodePriority(st_opmode volatile *pst_opmode) {
-    if      (pst_opmode->u8_SW1 != 0)   return OPMODE_KEYBOARD; // priority via SW1
-    else if (pst_opmode->u8_SW2 != 0)   return OPMODE_RM_NON_ALPHA; // downwards
-    else if (pst_opmode->u8_SW3 != 0)   return OPMODE_TO_UPPER;
-    else if (pst_opmode->u8_SW4 != 0)   return OPMODE_ENCRYPT;
-    else if (pst_opmode->u8_SW5 != 0)   return OPMODE_DECRYPT;
+    if      (pst_opmode->u16t_SW1 != 0)   return OPMODE_KEYBOARD; // priority via SW1
+    else if (pst_opmode->u16t_SW2 != 0)   return OPMODE_RM_NON_ALPHA; // downwards
+    else if (pst_opmode->u16t_SW3 != 0)   return OPMODE_TO_UPPER;
+    else if (pst_opmode->u16t_SW4 != 0)   return OPMODE_ENCRYPT;
+    else if (pst_opmode->u16t_SW5 != 0)   return OPMODE_DECRYPT;
     else                                return OPMODE_ECHO; // default
 }
