f6390539830489fd62f5eee6f9dae938963fc16a
imacole <129080386+imacole@users.noreply.github.com>
2023-04-15 16:04:13 -0500

    Update esos_stm32l4_ptb_sui.c

--- local/esos_stm32l4_ptb_sui.c
+++ local/esos_stm32l4_ptb_sui.c
@@ -30,23 +30,23 @@ void esos_hw_sui_configLED ( ESOS_SUI_LED_HANDLE h_LED ) {
 }
 
 
-// ESOS returns the process for the user to turn on LEDs 1/2 or both through the SUI
+// ESOS returns the process for the user to turn on LEDs 0-4 through the SUI
 void esos_hw_sui_turnLEDOn ( ESOS_SUI_LED_HANDLE h_LED ) {
     gpio_set(esos_sui_getLEDUserData1(h_LED), esos_sui_getLEDUserData2(h_LED));
 }
 
 
-// ESOS returns the process for the user to turn off LEDs 1/2 or both through the SUI
+// ESOS returns the process for the user to turn off LEDs 0-4 through the SUI
 void esos_hw_sui_turnLEDOff ( ESOS_SUI_LED_HANDLE h_LED ) {
     gpio_clear(esos_sui_getLEDUserData1(h_LED), esos_sui_getLEDUserData2(h_LED));
 }
 
-// SUI response to user input to toggle LEDs 1/2 or both
+// SUI response to user input to toggle LEDs 0-4
 void esos_hw_sui_toggleLED ( ESOS_SUI_LED_HANDLE h_LED ) {
     gpio_toggle(esos_sui_getLEDUserData1(h_LED), esos_sui_getLEDUserData2(h_LED));
 }
 
-// ESOS calls back to user to choose what switches needs initialization, LEDUserData1/2 in this case
+// ESOS calls back to user to choose what switches needs initialization, SwitchUserData0-4 in this case
 void esos_hw_sui_configSwitch ( ESOS_SUI_SWITCH_HANDLE h_SWITCH ) {
     gpio_mode_setup(esos_sui_getSwitchUserData1(h_SWITCH), GPIO_MODE_INPUT, GPIO_PUPD_NONE, esos_sui_getSwitchUserData2(h_SWITCH));
 }
